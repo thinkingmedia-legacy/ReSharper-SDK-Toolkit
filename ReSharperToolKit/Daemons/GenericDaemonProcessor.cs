bf838f7052ea581612a724825235d9dfaace4213
thinkingmedia <mathew@thinkingmedia.ca>
2014-06-25 15:21:13 -0400

    some base classes for daemons.

---
+++ ReSharperToolKit/Daemons/GenericDaemonProcessor.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.ReSharper.Daemon;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.Files;
+using JetBrains.ReSharper.Psi.Tree;
+
+namespace ReSharperToolKit.Daemons
+{
+    public abstract class GenericDaemonProcessor<TNodeType, TLanguageType, TFileType> : IDaemonStageProcess
+        where TNodeType : class, ITreeNode
+        where TLanguageType : PsiLanguageType
+        where TFileType : class, IFile
+    {
+        /// <summary>
+        /// The current process
+        /// </summary>
+        private readonly IDaemonProcess _process;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        protected GenericDaemonProcessor(IDaemonProcess pProcess)
+        {
+            _process = pProcess;
+        }
+
+        /// <summary>
+        /// Derived classes provide highlighting info when TreeNodes are found for the given language and file type.
+        /// </summary>
+        protected abstract IEnumerable<HighlightingInfo> getHighlights(TFileType pFile, TNodeType pNode);
+
+        /// <summary>
+        /// Executes the process.
+        /// </summary>
+        public void Execute(Action<DaemonStageResult> pCommitter)
+        {
+            if (!_process.FullRehighlightingRequired)
+            {
+                return;
+            }
+
+            List<HighlightingInfo> highlights = new List<HighlightingInfo>();
+            IPsiSourceFile sourceFile = _process.SourceFile;
+            IPsiServices psiServices = sourceFile.GetPsiServices();
+            TFileType file = psiServices.Files.GetDominantPsiFile<TLanguageType>(sourceFile) as TFileType;
+            if (file == null)
+            {
+                return;
+            }
+
+            file.ProcessChildren<TNodeType>(pNode=>highlights.AddRange(getHighlights(file, pNode)));
+
+            if (highlights.Count == 0)
+            {
+                return;
+            }
+
+            pCommitter(new DaemonStageResult(highlights));
+        }
+
+        /// <summary>
+        /// Whole daemon process
+        /// </summary>
+        public IDaemonProcess DaemonProcess
+        {
+            get { return _process; }
+        }
+    }
+}
---
+++ ReSharperToolKit/Daemons/GenericDaemonStage.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Application.Settings;
+using JetBrains.ReSharper.Daemon;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.Files;
+using JetBrains.ReSharper.Psi.Tree;
+
+namespace ReSharperToolKit.Daemons
+{
+    /// <summary>
+    /// A reusable base for creating daemon processes for a specific language and file type.
+    /// </summary>
+    public abstract class GenericDaemonStage<TLanguageType, TFileType> : IDaemonStage
+        where TLanguageType : PsiLanguageType
+        where TFileType : class, IFile
+    {
+        /// <summary>
+        /// Called to create processors only when a C sharp file is being processed.
+        /// </summary>
+        protected abstract IEnumerable<IDaemonStageProcess> CreateProcessByFile(IDaemonProcess pProcess,
+                                                                                IContextBoundSettingsStore pSettings,
+                                                                                DaemonProcessKind pProcessKind,
+                                                                                TFileType pFile);
+
+        /// <summary>
+        /// Creates processors for C sharp files.
+        /// </summary>
+        public IEnumerable<IDaemonStageProcess> CreateProcess(IDaemonProcess pProcess,
+                                                              IContextBoundSettingsStore pSettings,
+                                                              DaemonProcessKind pProcessKind)
+        {
+            IPsiSourceFile sourceFile = pProcess.SourceFile;
+            IPsiServices psiServices = sourceFile.GetPsiServices();
+            TFileType file = psiServices.Files.GetDominantPsiFile<TLanguageType>(sourceFile) as TFileType;
+
+            return file == null
+                ? Enumerable.Empty<IDaemonStageProcess>()
+                : CreateProcessByFile(pProcess, pSettings, pProcessKind, file);
+        }
+
+        /// <summary>
+        /// Check the error stripe indicator necessity for this stage after processing given <paramref name="pSourceFile"/>
+        /// </summary>
+        public ErrorStripeRequest NeedsErrorStripe(IPsiSourceFile pSourceFile, IContextBoundSettingsStore pSettingsStore)
+        {
+            return ErrorStripeRequest.STRIPE_AND_ERRORS;
+        }
+    }
+}
